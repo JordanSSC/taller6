9fb09a3f29ff986b02bbca2bb6a5c155a734330e
Diego Maldonado <149981678+dimaboni@users.noreply.github.com>
2024-12-11 15:55:18 -0500

    BuscadorEmpleado refactorizado
    Extract method

--- src/BuscadorEmpleado.java
+++ src/BuscadorEmpleado.java
@@ -1,13 +1,18 @@
 import java.util.List;
 
+//Extract Method
 public class BuscadorEmpleado {
     public static Empleado buscarEmpleadoPorNombre(String nombre, List<Empleado> empleados) {
         for (Empleado empleado : empleados) {
-            if (empleado.getNombre().equals(nombre)) {
+            if (esEmpleado(empleado,nombre)) { //linea cambiada con un call del nuevo metodo esEmpleado
                 return empleado;
             }
         }
         return null;
+    }
+    
+    private static boolean esEmpleado(Empleado empleado, String nombre) {
+        return empleado.getNombre().equals(nombre); //linea original
     }
 
     // Más metodos
